27e6518f8bdd64eb2c6509a31b291ce0ebea6675
lidongyun <1037083108@qq.com>
2022-06-22 22:39:24 +0800

    feat(app.vue): 2022-6-22 22.39

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+
+// 全局引入ui库
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+import store from './store'
+
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.use(ElementPlus)
+app.mount('#app')
+
+// createApp(App).mount('#app')
