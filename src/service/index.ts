aec4069f32b4c2fb1f2624599e44ba6b0e523dbf
lidongyun <1037083108@qq.com>
2022-07-01 00:09:10 +0800

    feat(service): 2022-6-30 12.00
    封装axios

--- src/service/index.ts
+++ src/service/index.ts
@@ -4,7 +4,21 @@ import { BASE_URL, TIME_OUT } from './request/config'
 
 const hyRequest = new HYRequest({
   baseURL: BASE_URL,
-  timeout: TIME_OUT
+  timeout: TIME_OUT,
+  interceptors: {
+    requestInterceptor: (config) => {
+      return config
+    },
+    requestInterceptorCatch: (error) => {
+      return error
+    },
+    responseInterceptor: (res) => {
+      return res
+    },
+    responseInterceptorCatch: (error) => {
+      return error
+    }
+  }
 })
 
 //与第一个请求因为baseURl不同所以产生了两个实例
--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,12 +1,24 @@
 import axios from 'axios'
 import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import type { HYRequestConfig, HYRequestInterceptors } from './type'
 
 //当传入的baseURL或者其他信息有变化的时候，通过创建两个实例的方法完成请求
 class HYRequest {
   instance: AxiosInstance
+  interceptors?: HYRequestInterceptors
 
-  constructor(config: AxiosRequestConfig) {
+  constructor(config: HYRequestConfig) {
     this.instance = axios.create(config)
+    this.interceptors = config.interceptors
+
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    )
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptor,
+      this.interceptors?.responseInterceptorCatch
+    )
   }
   request(config: AxiosRequestConfig): void {
     this.instance.request(config).then((res) => {
@@ -15,5 +27,7 @@ class HYRequest {
   }
   // get(){}
 }
+
+//拦截器的作用：封装一些公有逻辑
 
 export default HYRequest
