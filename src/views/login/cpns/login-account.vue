da44b01ef79d313363e3b8eab8725a175fe56dc7
lidongyun <1037083108@qq.com>
2022-07-14 23:40:49 +0800

    feat(login): 2022-7-14 23.40
    修改登录界面样式，但是tabs的头部样式有问题

--- src/views/login/cpns/login-account.vue
+++ src/views/login/cpns/login-account.vue
@@ -14,40 +14,14 @@
 <script lang="ts">
 import { defineComponent, reactive } from 'vue'
 
+import { rules } from '../config/account-config'
+
 export default defineComponent({
   setup() {
     const account = reactive({
       name: '',
       password: ''
     })
-
-    //编写校验规则
-    const rules = {
-      name: [
-        {
-          required: true,
-          message: '用户名是必传内容',
-          trigger: 'blur'
-        },
-        {
-          pattern: /^[a-z0-9]{5,10}$/,
-          message: '用户名必须是5-10个字母或者数字',
-          trigger: 'blur'
-        }
-      ],
-      password: [
-        {
-          required: true,
-          message: '密码是必传内容',
-          trigger: 'blur'
-        },
-        {
-          pattern: /^[a-z0-9]{3,}$/,
-          message: '密码必须是3位以上的字母或者数字',
-          trigger: 'blur'
-        }
-      ]
-    }
 
     return {
       account,
--- src/views/login/cpns/login-panel.vue
+++ src/views/login/cpns/login-panel.vue
@@ -21,11 +21,18 @@
         <login-phone />
       </el-tab-pane>
     </el-tabs>
+
+    <div class="account-control">
+      <el-checkbox v-model="isKeepPassWord">记住密码</el-checkbox>
+      <el-link type="primary">忘记密码</el-link>
+    </div>
+
+    <el-button type="primary" class="login-btn">立即登录</el-button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import loginAccount from './login-account.vue'
 import LoginPhone from './login-phone.vue'
 
@@ -35,7 +42,11 @@ export default defineComponent({
     LoginPhone
   },
   setup() {
-    return {}
+    const isKeepPassWord = ref(true)
+
+    return {
+      isKeepPassWord
+    }
   }
 })
 </script>
@@ -48,5 +59,23 @@ export default defineComponent({
   .title {
     text-align: center;
   }
+
+  .account-control {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+  }
+
+  .login-btn {
+    width: 100%;
+    margin-top: 10px;
+  }
+}
+
+.el-tabs--card ::v-deep .el-tabs__item {
+  margin: 1px 0 0 1px;
+}
+.el-tabs--card ::v-deep .el-tabs__header {
+  margin: 0;
 }
 </style>
